07c756319971407bd421057aabbfbc0deb496855
Giuseppe <132507608+GiuseppeTaylorr@users.noreply.github.com>
2023-11-16 08:46:33 +0000

    faltaterminar

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom";
 
 const Card = (props) => {
   return (
@@ -6,13 +7,13 @@ const Card = (props) => {
       <div className="card" style={{ width: "18rem" }}>
         <img src={props.src} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{props.Name}</h5>
+          <h5 className="card-title">{props.name}</h5>
           <p className="card-text">{props.gender}</p>
           <p className="card-text">{props.hairColor}</p>
           <p className="card-text">{props.eyeColor}</p>
           <div className="row">
             <div className="col-6">
-              <button className="btn btn-primary">Learn More</button>
+              <Link to={`/characters/${props.item_id}`} className="btn btn-primary">Learn More</Link>
             </div>
             <div className="col-6">
               <button className="btn btn-warning"><i className="bi bi-heart "></i></button>
@@ -20,7 +21,7 @@ const Card = (props) => {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,97 +2,56 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import Card from "../component/card";
 import "../../styles/home.css";
+import Planets from "../component/planets";
+
 
 export const Home = () => {
 
 	const { store } = useContext(Context)
 	return (
-		<div className="container mt-5">
-			<h1 className="text-danger">Characters</h1>
-			<div className="carrusel">
-				<div className="row">
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-				</div>
+		<>
+			<div className="container ">
+				<h1 className="text-danger">Characters</h1>
+				<div className="carrusel">
+					{store.characters.map((item) => {
+						return (
+							<div className="row">
+								<div className="col-1">
+									<Card src={`https://starwars-visualguide.com/assets/img/characters/${item.uid}.jpg`} alt=""
+										name={item.properties.name}
+										gender={item.properties.gender}
+										hairColor={item.properties.hair_color}
+										eyeColor={item.properties.eye_color}
+									/>
+								</div>
 
-			</div>
-			<h1 className="text-danger">Planets</h1>
-			<div className="carrusel">
-				<div className="row">
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
-					<div className="col-1">
-						<Card src="https://img.freepik.com/foto-gratis/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco_125540-3378.jpg?w=996&t=st=1699512050~exp=1699512650~hmac=ba2371dcf314320afa76f17e249427bb5109fbcee202a6245e54353568b2dc6f" Name="Luke" Gender="Male" hairColor="blond" eyeColor="blue" />
-					</div>
+							</div>
+						)
+					})
+					}
 				</div>
-
 			</div>
-		</div>
+			<div className="container mt-5">
+				<h1 className="text-danger">Planets</h1>
+				<div className="carrusel">
+					{store.planets.map((item) => {
+						return (
+							<div className="row">
+								<div className="col-1">
+									<Planets
+										src={`https://starwars-visualguide.com/assets/img/planets/${item.uid}.jpg`}
+										alt="planets"
+										population={item.properties.population}
+										terrain={item.properties.terrain}
+									/>
+								</div>
+							</div>
+						)
+					})}
+
+				</div>
+			</div>
+		</>
 	);
 }
 
